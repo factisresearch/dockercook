29ed19fb9e6392c8301ac248babb3a37d5509404
David Leuschner <leuschner@factisresearch.com>
2014-08-12 17:12:40 +0200

    fixed unpacking of tars (keeping a file is not an error)

--- src/lib/Cook/Build.hs
+++ src/lib/Cook/Build.hs
@@ -108,7 +108,7 @@ buildImage mStreamHook cfg@(CookConfig{..}) stateManager fileHashes bf =
                      BSC.concat
                      [ "COPY context.tar.gz /context.tar.gz\n"
                      , "RUN mkdir -p ", BSC.pack target, "\n"
-                     , "RUN tar xvkf /context.tar.gz -C ", BSC.pack target, "\n"
+                     , "RUN tar xvk --skip-old-files -f /context.tar.gz -C ", BSC.pack target, "\n"
                      , "RUN rm -rf /context.tar.gz\n"
                      ]
            dockerBS =
